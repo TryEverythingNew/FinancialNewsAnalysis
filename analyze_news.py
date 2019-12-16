ce25b18d797c32bb2e6760c8bfa2c6585d2006be
xueliangsun <xueliang.sun@wsu.edu>
2019-12-15 23:52:23 -0500

    Add test data set, complet classification

--- analyze_news.py
+++ analyze_news.py
@@ -1,29 +1,16 @@
 import metapy
 
-def count_word(doc):
-    #Write a token stream that tokenizes with ICUTokenizer,
-    #lowercases, removes words with less than 2 and more than 30 characters
-    #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams"
-    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
-    tok = metapy.analyzers.LowercaseFilter(tok)
-    tok = metapy.analyzers.LengthFilter(tok, min=2, max=30)
-    tok = metapy.analyzers.Porter2Filter(tok)
-    tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)
-    ana = metapy.analyzers.NGramWordAnalyzer(1, tok)
-    trigrams = ana.analyze(doc)
-    tok.set_content(doc.content())
-    tokens, counts = [], []
-    return {token: count for token, count in trigrams.items()}
-
 train_idx = metapy.index.make_forward_index('train.toml')
-with open('./scraper_code/news.txt', 'r') as file:
-    news_list = [line.strip() for line in file.readlines()]
-with open('./scraper_code/news_url.txt', 'r') as file:
-    url_list = [line.strip() for line in file.readlines()]
-assert len(url_list) == len(news_list), "The number of urls mismatchs the number of news"
+train_dset = metapy.classify.MulticlassDataset(train_idx)
 
-for url, news in zip(url_list, news_list):
-    doc = metapy.index.Document()
-    doc.content(news)
-    
+np = metapy.classify.NaiveBayes(train_dset)
+print("classify for traning data", flush=True)
+for train in train_dset:
+    print(f"Actual label is {train_dset.label(train)}, classified to {np.classify(train.weights)}")
 
+with open('test_news/news_url.txt', 'r') as file:
+    url_list = file.readlines()
+test_idx = metapy.index.make_forward_index('test.toml')
+print("classify for test data, test data might not be labeled", flush=True)
+for url, test in zip(url_list, metapy.classify.MulticlassDataset(test_idx)):
+    print(f"URL {url} is classified to {np.classify(test.weights)}")
